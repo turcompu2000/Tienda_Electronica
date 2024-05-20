81b0d57fc4e362ffcdb8ab0c9df9df90e71362e3
Jhorman montoya <Jhormanmontoya@campuscecep.edu.co>
2024-05-20 14:44:27 -0500

    Productos Rutas API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ProductosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/productos', [ProductosController::class, 'store'])->name('productos.store');
+Route::get('/productos', [ProductosController::class, 'index'])->name('productos');
+Route::delete('/productos/{producto}', [ProductosController::class, 'destroy'])->name('productos.destroy');
+Route::get('/productos/{producto}', [ProductosController::class, 'show'])->name('productos.show');
+Route::put('/productos/{producto}', [ProductosController::class, 'update'])->name('productos.update');
